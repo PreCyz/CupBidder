30e5590b1728483422479aecf4a429f0a8d8e528
Paweł Gawędzki <precpaw@op.pl>
2017-05-07 19:05:36 +0200

    Organizing imports.

--- backend/src/main/java/bidder/configs/MainConfig.java
+++ backend/src/main/java/bidder/configs/MainConfig.java
@@ -1,7 +1,7 @@
 package bidder.configs;
 
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /** Created by gawa on 01.05.17. */
--- backend/src/main/java/bidder/repositories/UserRepository.java
+++ backend/src/main/java/bidder/repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package bidder.repositories;
 
 import bidder.model.users.*;
-import org.springframework.data.mongodb.repository.*;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 

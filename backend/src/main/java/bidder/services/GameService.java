8b6a1e3f6fb4d6440ad56c3775d5d8a989e4c598
Paweł Gawędzki <precpaw@op.pl>
2017-05-08 23:07:33 +0200

    Adding GameServiceImpl.

--- backend/src/main/java/bidder/services/GameService.java
+++ backend/src/main/java/bidder/services/GameService.java
@@ -1,5 +1,14 @@
 package bidder.services;
 
+import bidder.model.match.Game;
+
+import java.util.List;
+
 /** Created by gawa on 06.05.17. */
 public interface GameService {
+
+	void dropGames();
+	Game addGame(Game game);
+	List<Game> addAllGames(List<Game> games);
+
 }
---
+++ backend/src/main/java/bidder/services/impl/GameServiceImpl.java
@@ -0,0 +1,32 @@
+package bidder.services.impl;
+
+import bidder.model.match.Game;
+import bidder.repositories.GameRepository;
+import bidder.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/** Created by gawa on 2017-05-08. */
+@Service
+public class GameServiceImpl implements GameService {
+
+	@Autowired
+	private GameRepository gameRepository;
+
+	@Override
+	public void dropGames() {
+		gameRepository.deleteAll();
+	}
+
+	@Override
+	public Game addGame(Game game) {
+		return gameRepository.save(game);
+	}
+
+	@Override
+	public List<Game> addAllGames(List<Game> games) {
+		return gameRepository.save(games);
+	}
+}

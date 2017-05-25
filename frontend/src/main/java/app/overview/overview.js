b8356b27c29882c9baea5d4c065e05682b3d2f8e
Gawa <precpaw@op.pl>
2017-05-25 11:56:20 +0200

    Displaying cups and their games in overview screen.

--- frontend/src/main/java/app/overview/overview.js
+++ frontend/src/main/java/app/overview/overview.js
@@ -1,7 +1,28 @@
-mainAppModule.controller('overviewCtrl', function($rootScope, $scope) {
+mainAppModule.controller('overviewCtrl', function($rootScope, $scope, $http) {
+    $http({
+        method : "GET",
+        url : "http://localhost:8080/api/cup"
+    }).then(function mySuccess(response) {
+        $scope.cups = response.data.cups;
+    }, function myError(response) {
+        $scope.requestErrorMsg = response.statusText;
+    });
+
     $rootScope.hideSignIn = true;
     $scope.logout = function() {
         console.log('logout() call.')
         $rootScope.showSignIn();
+    }
+
+    $scope.showGame = false;
+    $scope.showGames = function(index) {
+        console.log('showGames() call. ' + index);
+        $scope.showGame = true;
+        $scope.games = $scope.cups[index].games;
+    }
+
+    $scope.hideGames = function() {
+        console.log('hideGames() call.');
+        $scope.showGame = false;
     }
 });
